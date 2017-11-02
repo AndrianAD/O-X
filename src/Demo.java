5e907fdcaa47c96b53f1a008afbd679100d71061
Андриан <uncroptv@gmail.com>
2017-11-02 22:18:49 +0200

    chek_win

--- src/Demo.java
+++ src/Demo.java
@@ -1,55 +1,155 @@
-/**
- * Created by User on 171024.
- */
+import java.util.Random;
 public class Demo {
-    private static int sum = 0;
+    public static int size = 3;     // size of field
+    public static int chek_win = 3; // number of points to win
 
+    //        public static int mas[][] = {{0,0,0,0}, {0,0,1,0},{0,1,0,0},{1,0,0,0}};
+    public static int mas[][] = newArray();
 
-    public static int GetSum(int a, int b) {
-        if (a == b) {
-            return a;
+    // chek if game is over
+    public void chek_win(int[][] masiv) {
+        for (int i = 0; i < masiv.length; i++) {
+            for (int j = 0; j < masiv.length; j++) {
+
+                if (masiv[i][j] == 1) {
+                    chek_rigth(i, j);
+                    chek_down(i, j);
+                    chek_diagonal_right(i, j);
+                    chek_diagonal_left(i, j);
+                }
+            }
         }
-        int min, max;
-        if (a > b) {
-            min = b;
-            max = a;
-        } else {
-            min = a;
-            max = b;
+    }
+
+    // check matching horizontaly
+    private boolean chek_rigth(int i, int j) {
+        if (((j + chek_win) > size)) {
+            return false;
         }
-        for (int i = min; i <= max; i++) {
-            sum += i;
+        int temp = chek_win - 1;
+        while (temp > 0) {
+            if (mas[i][j + temp] == 1) {
+                temp--;
+            } else break;
+
         }
-        return sum;
+        if (temp == 0) {
+            System.out.println("совпадение по горизонтали");
+            return true;
+        }
+        return false;
     }
 
 
-//    public static String getMiddle(String word) {
-//        int length = word.length();
-//        if (length != 1) {
-//            if (length % 2 == 0) {
-//                String result = String.valueOf((word.charAt(length / 2 - 1)));
-//                result += String.valueOf((word.charAt(length / 2)));
-//                return result;
-//            } else {
-//                String result = String.valueOf((word.charAt((int) Math.floor(length / 2))));
-//                return result;
-//            }
-//        }
-//        return word;
-//    }
-
-
-    public static String getMiddle(String word) {
-        //Code goes here!
-        while(word.length() > 2) {
-            word = word.substring(1, word.length()-1);
+    // check matching vertically
+    private boolean chek_down(int i, int j) {
+        if (((i + size - 1) >= size)) {
+            return false;
         }
+        int temp = chek_win - 1;
+        while (temp > 0) {
+            if (mas[i + temp][j] == 1) {
+                temp--;
+            } else break;
 
-        return word;
+        }
+        if (temp == 0) {
+            System.out.println("совпадение по вертикали");
+            return true;
+        }
+        return false;
+
     }
 
+    // check matching diagonal on the right
+    private boolean chek_diagonal_right(int i, int j) {
+        int temp = chek_win - 1;
 
+        if (((j + size - 1) >= size)) {
+            return false;
+        }
+
+        if (((i + size - 1) >= size)) {
+            return false;
+        }
+
+
+        while (temp > 0) {
+            if (mas[i + temp][j + temp] == 1) {
+                temp--;
+            } else break;
+
+        }
+        if (temp == 0) {
+            System.out.println("совпадение по диагонали в право");
+        }
+
+        return false;
+    }
+
+    // check matching diagonal on the left
+    private boolean chek_diagonal_left(int i, int j) {
+        int temp = chek_win - 1;
+// check left border
+        if (j + 1 - chek_win < 0)
+            return false;
+//    check down border
+        if (((i + size - 1) >= size)) {
+            return false;
+        }
+        while (temp > 0) {
+            if (mas[i + temp][j - temp] == 1) {
+                temp--;
+            } else break;
+
+        }
+        if (temp == 0) {
+            System.out.println("совпадение по диагонали в лево");
+        }
+        return false;
+    }
+
+    // make new array with size
+    private static int[][] newArray() {
+        int mas[][] = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                mas[i][j] = 0;
+                System.out.print(mas[i][j]);
+            }
+            System.out.println();
+        }
+        return mas;
+    }
+
+    // put in Array a random one 1
+    public void random_chose(int[][] masiv) {
+        System.out.println("------------------------------------");
+        ;
+        boolean chek = false;
+
+        while (chek != true) {
+            int random_numberI = new Random().nextInt(masiv.length);
+            int random_numberJ = new Random().nextInt(masiv.length);
+            if (mas[random_numberI][random_numberJ] == 0) {
+                mas[random_numberI][random_numberJ] = 1;
+                chek = true;
+            }
+        }
+        printArray(mas);
+        System.out.println("------------------------------------");
+
+    }
+
+    // print Array
+    public void printArray(int masiv[][]) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(masiv[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 
 
